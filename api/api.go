a8fd97f5bc3a13ee776dd53d73d6bc3ca8be85f5
Dendi <dendiaa@students.amikom.ac.id>
2022-11-25 02:04:06 +0700

    feat: route

--- api/api.go
+++ api/api.go
@@ -2,34 +2,44 @@ package api
 
 import (
 	"fmt"
+	"net/http"
 
 	"github.com/DendiAnugerah/Todo/repository"
-	"github.com/labstack/echo/v4"
 )
 
 type API struct {
 	TodoRepo    repository.TodoRepository
 	UserRepo    repository.UserRepository
 	SessionRepo repository.SessionRepository
+	mux         *http.ServeMux
 }
 
 func NewAPI(todoRepo repository.TodoRepository, userRepo repository.UserRepository, sessionRepo repository.SessionRepository) API {
-	api := API{todoRepo, userRepo, sessionRepo}
-	c := echo.New()
+	mux := http.NewServeMux()
+	api := API{
+		todoRepo,
+		userRepo,
+		sessionRepo,
+		mux,
+	}
 
-	c.POST("/user/register", api.RegisterUser)
-	c.POST("/user/login", api.Login)
-	// c.GET("/user/logout", api.Logout)
+	mux.Handle("/user/register", api.Post(http.HandlerFunc(api.Register)))
+	mux.Handle("/user/login", api.Post(http.HandlerFunc(api.Login)))
+	mux.Handle("/user/logout", api.Get(api.Auth(http.HandlerFunc(api.Logout))))
+
+	mux.Handle("/todo/add", api.Post(api.Auth(http.HandlerFunc(api.addTodo))))
+	mux.Handle("/todo/remove", api.Delete(api.Auth(http.HandlerFunc(api.deleteTodo))))
+	mux.Handle("/todo/change-status", api.Put(api.Auth(http.HandlerFunc(api.changeTodoStatus))))
+	mux.Handle("/todo/list", api.Get(api.Auth(http.HandlerFunc(api.listTodo))))
 
 	return api
 }
 
-func (A *API) Handler() *echo.Echo {
-	return API
+func (A *API) Handler() *http.ServeMux {
+	return A.mux
 }
-func (A *API) Start() {
-	e := echo.New()
 
-	fmt.Println("Starting server on port 8080")
-	e.Logger.Fatal(e.Start(":8080"))
+func (A *API) Start() {
+	fmt.Println("starting web server at http://localhost:8080")
+	http.ListenAndServe(":8080", A.Handler())
 }
