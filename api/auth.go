0fd86bea445fd49086baca79f523493698471faf
Dendi <dendiaa@students.amikom.ac.id>
2022-11-25 02:02:09 +0700

    feat: middleware

--- api/auth.go
+++ api/auth.go
@@ -55,3 +55,25 @@ func (A *API) Post(next http.Handler) http.Handler {
 		next.ServeHTTP(w, r)
 	})
 }
+
+func (api *API) Delete(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodDelete {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Method is not allowed!"})
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (api *API) Put(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPut {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Method is not allowed!"})
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
+}
