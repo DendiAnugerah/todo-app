94b66ded3f34d4cb00794dcb0e775ab526f9b00a
Dendi <dendiaa@students.amikom.ac.id>
2022-11-25 01:56:51 +0700

    fix: todo api

--- api/todoAPI.go
+++ api/todoAPI.go
@@ -3,75 +3,92 @@ package api
 import (
 	"encoding/json"
 	"fmt"
+	"net/http"
 	"strconv"
 
 	"github.com/DendiAnugerah/Todo/model"
-	"github.com/labstack/echo/v4"
 )
 
-func (A *API) addTodo(c echo.Context) error {
-	username := fmt.Sprintf("%s", c.Request().Context().Value("username"))
+func (A *API) addTodo(w http.ResponseWriter, r *http.Request) {
+	username := fmt.Sprintf("%s", r.Context().Value("username"))
 
 	var todo model.Todo
-	err := json.NewDecoder(c.Request().Body).Decode(&todo)
+	err := json.NewDecoder(r.Body).Decode(&todo)
 	if err != nil {
-		return c.JSON(400, model.ErrorResponse{Error: err.Error()})
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Internal Server Error"})
+		return
 	}
 
 	err = A.TodoRepo.AddTodo(todo)
 	if err != nil {
-		return c.JSON(500, model.ErrorResponse{Error: err.Error()})
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Internal Server Error"})
+		return
 	}
 
-	return c.JSON(200, model.SuccessResponse{Username: username, Message: fmt.Sprintf("Task %s added!", todo.Task)})
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(model.SuccessResponse{Username: username, Message: fmt.Sprintf("Task %s added!", todo.Task)})
 }
 
-func (A *API) listTodo(c echo.Context) error {
+func (A *API) listTodo(w http.ResponseWriter, r *http.Request) {
 	res, err := A.TodoRepo.ReadTodo()
 	if err != nil {
-		return c.JSON(500, model.ErrorResponse{Error: err.Error()})
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: err.Error()})
+		return
 	}
 
 	if len(res) == 0 {
-		return c.JSON(200, model.SuccessResponse{Message: "No task found!"})
+		w.WriteHeader(http.StatusNotFound)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Todo list not found!"})
+		return
 	}
 
-	c.JSON(200, res)
-	return json.NewEncoder(c.Response()).Encode(res)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(res)
 }
 
-func (A *API) changeTodoStatus(c echo.Context) error {
-	username := fmt.Sprintf("%s", c.Request().Context().Value("username"))
+func (A *API) changeTodoStatus(w http.ResponseWriter, r *http.Request) {
+	username := fmt.Sprintf("%s", r.Context().Value("username"))
 
-	var model model.ToggleTodoReq
-	err := json.NewDecoder(c.Request().Body).Decode(&model)
+	var toggleTodo model.ToggleTodoReq
+	err := json.NewDecoder(r.Body).Decode(&toggleTodo)
 	if err != nil {
-		return c.JSON(400, nil)
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Internal Server Error"})
+		return
 	}
 
-	err = A.TodoRepo.UpdateTodoStatus(model.ID, model.Done)
+	err = A.TodoRepo.UpdateTodoStatus(toggleTodo.ID, toggleTodo.Done)
 	if err != nil {
-		return c.JSON(500, nil)
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Internal Server Error"})
+		return
 	}
 
-	c.JSON(200, nil)
-	return json.NewEncoder(c.Response()).Encode(username)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(model.SuccessResponse{Username: username, Message: "Status Changed"})
 }
 
-func (A *API) deleteTodo(c echo.Context) error {
-	username := fmt.Sprintf("%s", c.Request().Context().Value("username"))
+func (A *API) deleteTodo(w http.ResponseWriter, r *http.Request) {
+	username := fmt.Sprintf("%s", r.Context().Value("username"))
 
-	id := c.Request().URL.Query().Get("id")
+	id := r.URL.Query().Get("id")
 	idInt, err := strconv.Atoi(id)
 	if err != nil {
-		return c.JSON(400, model.ErrorResponse{Error: err.Error()})
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Internal Server Error"})
+		return
 	}
 
 	err = A.TodoRepo.DeleteTodo(idInt)
 	if err != nil {
-		return c.JSON(500, model.ErrorResponse{Error: err.Error()})
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(model.ErrorResponse{Error: "Internal Server Error"})
+		return
 	}
 
-	c.JSON(200, nil)
-	return json.NewEncoder(c.Response()).Encode(username)
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(model.SuccessResponse{Username: username, Message: "Delete Success"})
 }
