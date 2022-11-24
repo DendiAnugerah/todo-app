b6021caac6061623bdd410d062adefa79333b207
Dendi <dendiaa@students.amikom.ac.id>
2022-11-25 02:29:31 +0700

    fix: route

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"github.com/DendiAnugerah/Todo/api"
+	"github.com/DendiAnugerah/Todo/config"
+	"github.com/DendiAnugerah/Todo/model"
+	"github.com/DendiAnugerah/Todo/repository"
+)
 
+func main() {
+	db := config.NewDB()
+
+	conn := db.Connection()
+	conn.AutoMigrate(&model.User{}, &model.Todo{}, &model.Session{})
+
+	usersRepo := repository.NewUserRepository(conn)
+	todosRepo := repository.NewTodoRepository(conn)
+	sessionsRepo := repository.NewSessionRepository(conn)
+
+	API := api.NewAPI(todosRepo, usersRepo, sessionsRepo)
+	API.Start()
 }
